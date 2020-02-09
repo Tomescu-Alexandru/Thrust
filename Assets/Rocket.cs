59632f332e6441e6b2f5a1d16d9980da41d52cb6
Tomescu <alexandrutomescu23@gmail.com>
2020-02-09 23:56:47 +0200

    Basic movement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,18 +21,18 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space)) //poate sa accelereze in timp ce se roteste
         {
-            print("Space");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if(Input.GetKey(KeyCode.A))
         {
-            print("A");
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            print("D");
+            transform.Rotate(Vector3.back);
         }
     }
 }
